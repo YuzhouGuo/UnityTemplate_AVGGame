f0ca565bc8a117dd9ec295a4046ef746142fe413
Yuzhou Guo <guoyuzhou@piasset.net>
2022-11-28 22:30:40 +0800

    visual script line node first draft

--- Assets/Script/VisualScript/VisualLine.cs
+++ Assets/Script/VisualScript/VisualLine.cs
@@ -6,6 +6,9 @@ using UnityEngine.Localization;
 using UnityEditor.Localization;
 
 namespace Assets.Script.VisualScript{
+
+    [UnitShortTitle("Line")]
+    [UnitCategory("AVG Game Nodes")]
     public class VisualLine : Unit{
         [DoNotSerialize]
         public ControlInput inputTrigger;
@@ -19,6 +22,24 @@ namespace Assets.Script.VisualScript{
         [DoNotSerialize]
         public ValueInput actor;
 
+        [DoNotSerialize]
+        public ValueInput ffamily;
+
+        [DoNotSerialize]
+        public ValueInput fcolor;
+
+        [DoNotSerialize]
+        public ValueInput fsize;
+
+        [DoNotSerialize]
+        public ValueInput linespacing;
+
+        [DoNotSerialize]
+        public ValueInput align;
+
+        [DoNotSerialize]
+        public ValueInput fstyle;
+
         protected override void Definition(){
             inputTrigger = ControlInput("in", (flow) => { return outputTrigger; });
             outputTrigger = ControlOutput("out");
@@ -29,6 +50,13 @@ namespace Assets.Script.VisualScript{
             // var entry = collection.SharedData.GetEntry("New Entry");
             stringTable = ValueInput<StringTableCollection>("stringTable", LocalizationEditorSettings.GetStringTableCollection("test"));
             actor = ValueInput<Actor>("actor", Actor.NULL);
+
+            ffamily = ValueInput<string>("ffamily", string.Empty);
+            fcolor = ValueInput<string>("fcolor", "0x6079d2ff");
+            fsize = ValueInput<float>("fsize", 15.0f);
+            linespacing = ValueInput<float>("linespacing", 0.0f);
+            align = ValueInput<Align>("align", Align.LT);
+            fstyle = ValueInput<FontStyle>("fstyle", FontStyle.Normal);         
         }
     }
 }
